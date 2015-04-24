5111a81d00acd422a00e36b902bb89facd5b9d01
Vitalie Cherpec <vitalie@penguin.ro>
2015-04-24 15:59:49 +0300

    Update started/completed messages

--- middleware_logger.go
+++ middleware_logger.go
@@ -19,7 +19,7 @@ func (l *Logger) Exec(sw StatusWriter, fact string, args *Args, next JobRunner) 
 	jinfo := fact + " " + args.String()
 	start := time.Now()
 
-	l.Println(jinfo, "... started")
+	l.Println(jinfo, "started ...")
 
 	next(sw, fact, args)
 
@@ -28,5 +28,5 @@ func (l *Logger) Exec(sw StatusWriter, fact string, args *Args, next JobRunner) 
 		status = fmt.Sprintf("FAIL (%v)", sw.Get())
 	}
 
-	l.Printf("%s ... in %v ... %v", jinfo, time.Since(start), status)
+	l.Printf("%s completed in %v ... %v", jinfo, time.Since(start), status)
 }
